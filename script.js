85538fbd8e55707cd3408bc388014f67a5845b24
Mohit Choudhary <166812059+94mohit@users.noreply.github.com>
2026-06-20 08:30:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const typed = new Typed("#typing", {
+    strings: [
+        "Aspiring Data Analyst",
+        "Python Enthusiast",
+        "SQL Developer",
+        "Power BI Learner"
+    ],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 1500,
+    loop: true
+});
