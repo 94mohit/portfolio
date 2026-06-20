47ab8b9169d703a078c378e99723035014af1e1f
Mohit Choudhary <166812059+94mohit@users.noreply.github.com>
2026-06-20 10:24:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-const typed = new Typed("#typing", {
-    strings: [
-        "Aspiring Data Analyst",
-        "Python Enthusiast",
-        "SQL Developer",
-        "Power BI Learner"
-    ],
-    typeSpeed: 60,
-    backSpeed: 40,
-    backDelay: 1500,
-    loop: true
+const navLinks = document.getElementById("navLinks");
+const menuBtn = document.querySelector(".menu-btn");
+
+// Menu toggle
+menuBtn.addEventListener("click", function (e) {
+    e.stopPropagation(); // click ko document tak mat jane do
+    navLinks.classList.toggle("active");
 });
 
-function toggleMenu() {
-    document.getElementById("navLinks").classList.toggle("active");
-}
+// Bahar click karne par menu band
+document.addEventListener("click", function () {
+    navLinks.classList.remove("active");
+});
 
-// Outside click se menu close
-document.addEventListener("click", function (event) {
-    const navLinks = document.getElementById("navLinks");
-    const menuBtn = document.querySelector(".menu-btn");
-
-    if (
-        !navLinks.contains(event.target) &&
-        !menuBtn.contains(event.target)
-    ) {
-        navLinks.classList.remove("active");
-    }
+// Menu ke andar click ho to band na ho
+navLinks.addEventListener("click", function (e) {
+    e.stopPropagation();
 });
