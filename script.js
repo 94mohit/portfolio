3a8a6abbd02fdf924a8a6778cbaa7a7dd16eab4c
Mohit Choudhary <166812059+94mohit@users.noreply.github.com>
2026-06-20 10:20:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ const typed = new Typed("#typing", {
     backDelay: 1500,
     loop: true
 });
+
 function toggleMenu() {
     document.getElementById("navLinks").classList.toggle("active");
 }
+
+// Outside click se menu close
+document.addEventListener("click", function (event) {
+    const navLinks = document.getElementById("navLinks");
+    const menuBtn = document.querySelector(".menu-btn");
+
+    if (
+        !navLinks.contains(event.target) &&
+        !menuBtn.contains(event.target)
+    ) {
+        navLinks.classList.remove("active");
+    }
+});
